9adcce3922c2c123434d3ef5da242ca803f9aad9
yansvassi <yana.vassilyev@outlook.com>
2025-03-25 13:16:08 -0400

    WOW search work, stat work

--- assignment2.py
+++ assignment2.py
@@ -29,7 +29,7 @@ class Diagram : # How do I get the whole layered aspect of it??
         self.objects = [] # for storing
 
     def __str__(self): # todo: property: size, height, width
-        return f"\n{self.name}\ndepth: {self.depth}\nheight: {self.height}\nwidth: {self.width} \nobjects: \n" + "\n".join(str(obj) for obj in self.objects)
+        return f'\n{self.name}\ndepth: {self.depth}\nheight: {self.height}\nwidth: {self.width} \nobjects: \n' + '\n'.join(str(obj) for obj in self.objects)
 
 class DiagramObject : # todo this needs to change, object type is the first, rest is attributes silly goose
 
@@ -38,176 +38,275 @@ class DiagramObject : # todo this needs to change, object type is the first, res
         self.truncated = None
         self.difficult = None
         self.attribs = {} #do this or no?
-        self.boundary = [] # xmin, ymin, xmax, ymax
+        self.boundary = {'xmin':None, 'ymin':None, 'ymax':None, 'xmax':None}# xmin, ymin, xmax, ymax
+        self.area = 0;
 
     def __str__(self) :
-        return f"\n\t{self.objecttype} \n\t truncated: {self.truncated} \n\t difficult: {self.difficult} \n\t boundaries: \n\t\txmin: {int(self.boundary[0].text)}\n\t\tymin: {int(self.boundary[1].text)}\n\t\txmax: {int(self.boundary[2].text)}\n\t\tymax: {int(self.boundary[3].text)}" #todo: iterate through attribs
+        return f'\n\t{self.objecttype} \n\t truncated: {self.truncated} \n\t difficult: {self.difficult} \n\t boundaries: \n\t\txmin: {int(self.boundary[0].text)}\n\t\tymin: {int(self.boundary[1].text)}\n\t\txmax: {int(self.boundary[2].text)}\n\t\tymax: {int(self.boundary[3].text)}' #todo: iterate through attribs
 
 diagrams = {} #keys will be file names, values diagrams
-invalidInputs = 0
+invalid_inputs = 0
 
 def parsexml(xmlfile, diagram) :
     if os.path.basename(xmlfile)[:-4] in diagrams : #correct key??
-        raise Exception(f"Invalid entry: {os.path.basename(xmlfile)[:-4]}.xml already loaded")
+        raise Exception(f'Invalid entry: {os.path.basename(xmlfile)[:-4]}.xml already loaded')
     tree = ET.parse(xmlfile)
     root = tree.getroot()
     if root.find('.//size') is not None:
-        sizetag = root.find('.//size') # todo: assuming perfect input???
-        diagram.width = int(sizetag.find('width').text) # what if doesnt have this attrib??
-        diagram.height = int(sizetag.find('height').text)
-        diagram.depth = int(sizetag.find('depth').text)
+        size_tag = root.find('.//size') # todo: assuming perfect input???
+        diagram.width = int(size_tag.find('width').text) # what if doesnt have this attrib??
+        diagram.height = int(size_tag.find('height').text)
+        diagram.depth = int(size_tag.find('depth').text)
     if root.find('.//object') is not None:
-        for object in root.findall('.//object'):
+        for obj_element in root.findall('.//object'):
             obj = DiagramObject()
-            obj.objecttype = object.find('name').text
-            obj.truncated = object.find('truncated').text
-            obj.difficult = object.find('difficult').text
-            for coord in object.find('bndbox'):
-                obj.boundary.append(coord)
+            obj.objecttype = obj_element.find('name').text
+            obj.truncated = obj_element.find('truncated').text
+            obj.difficult = obj_element.find('difficult').text
+            for coord in obj_element.find('.//bndbox'):
+                obj.boundary[coord.tag] = coord.text
+            obj.area = (abs(obj.boundary['ymax'] - obj.boundary['ymin']) * abs(obj.boundary['xmax'] - obj.boundary['xmin']))
             diagram.objects.append(obj)
             # todo: add all the attribs??
 
 
 def main():
-    global invalidInputs
+    global invalid_inputs
 
     if len(sys.argv) != 2:
-        print("Usage: python3 your_script.py </path/to/xml/folder/>")
+        print('Usage: python3 your_script.py </path/to/xml/folder/>')
         sys.exit(1)
 
     directory = sys.argv[1]
 
     if not os.path.isdir(directory):
-        print("Invalid directory")
+        print('Invalid directory')
         sys.exit(1)
 
     while True:
-        input("Press Enter to continue...")
+        input('Press Enter to continue...')
 
-        print("""
+        print('''
         1. List Current Files
         2. List Diagrams
         3. Load File
         4. Display Diagram Info
         5. Search
         6. Statistics
-        7. Exit""")
+        7. Exit''')
 
-        response = input("\nYour selection: ")
+        response = input('\nYour selection: ')
 
         match response:
-            case "1":
-                allfiles = os.listdir(directory)
-                print("\nEnclosed files: ") #filter out only xmls?
+            case '1':
+                all_files = os.listdir(directory)
+                print('\nEnclosed files: ') #filter out only xmls?
                 counter = 1
-                for xml in allfiles:
+                for xml in all_files:
                     if xml.endswith('.xml'):
-                        print(" ",counter, ".", xml)
+                        print(' ',counter, '.', xml)
                         counter += 1
                 continue
-            case "2":
+            case '2':
                 if len(diagrams) == 0:
-                    print("0 diagrams loaded.")
+                    print('0 diagrams loaded.')
                 else:
-                    print(len(diagrams), "diagram(s) loaded: ")
-                    print(", ".join(diagrams.keys())) # remove line breaks todo: '' around each elem
+                    print(len(diagrams), 'diagram(s) loaded: ')
+                    print(', '.join(diagrams.keys())) # remove line breaks todo: '' around each elem
                 continue
-            case "3":
-                filename = input("Enter the filename to load: ")
+            case '3':
+                filename = input('Enter the filename to load: ')
                 #check if file available, if not return
-                pathtofile = directory + "/" + filename
-                if not os.path.isfile(pathtofile): #at this point are we in directory or do I have to put the whole path here?
-                    print("nuh uh! ")
+                path_to_file = directory + '/' + filename
+                if not os.path.isfile(path_to_file): #at this point are we in directory or do I have to put the whole path here?
+                    print('nuh uh! ')
                 else:
                     diagram = Diagram(filename[:-4])
                     try:
-                        parsexml(pathtofile, diagram) # this will be our value
+                        parsexml(path_to_file, diagram) # this will be our value
                         diagrams[filename[:-4]] = diagram
-                        print(filename + " successfully loaded")
+                        print(filename + ' successfully loaded')
                     except Exception as e:
                         print(e)
                 continue
-            case "4":
-                filename = input("Enter the filename to display: ") #only those that are already loaded right?, specify what format to type name of diagram (w xml?)
+            case '4':
+                filename = input('Enter the filename to display: ') #only those that are already loaded right?, specify what format to type name of diagram (w xml?)
                 if filename[:-4] in diagrams:
                     print(diagrams[filename[:-4]])
                 else:
-                    print(f"{filename} not found in loaded diagrams")
+                    print(f'{filename} not found in loaded diagrams')
                 continue
-            case "5":
-                print("5")
-                response2 = input("Would you like to search by type (1) or by dimension (2)? ")
-                match response2:
-                    case "1":
-                        print("search by type...")
-                        continue
-                    case "2":
+            case '5':
+                response_2 = input('Would you like to search by type (1) or by dimension (2)? ')
+                match response_2:
+                    case '1':
+                        search_type = input('Enter the diagram type: ')
                         found = {}
 
-                        print("\nSearch parameters \n---------------")
-                        minwidth = input("Min width (enter blank for 0): ")
-                        maxwidth = input("Max width (enter blank for max): ")
-                        minheight = input("Min height (enter blank for zero): ")
-                        maxheight = input("Max height (enter blank for max): ")
-                        difficult = input("Difficult (yes/no/All): ")
-                        truncated = input("Truncated (yes/no/All): ")
+                        for name, diagram in diagrams.items():
+                            for obj in diagram.objects:
+                                if obj.objecttype.lower() == search_type.lower():
+                                    found[name] = diagram
 
-                        for diagram in diagrams:
-                            if minwidth:
-                                if diagram.width >= minwidth:
-                                    continue #?
-                                else:
-                                    break #? onto next digram to check
-                            if maxwidth:
-                                if diagram.width <= maxwidth:
-                                    continue # this be good, next param
-                                else:
-                                    break
-                            if minheight:
-                                if diagram.height >= minheight:
-                                    continue
-                                else:
-                                    break
-                            if maxheight:
-                                if diagram.height <= maxheight:
-                                    found[diagram.name] = diagrams[diagram]
-                                else:
-                                    break
                         if len(found) > 0:
-                            print(f"Found {len(found)} diagrams: ")
-                            print(", \n".join(diagrams.keys()))
+                            print(f'\nFound {len(found)} diagrams: ')
+                            print(', \n'.join(found.keys()))
                         else:
-                            print("No matching diagrams found")
+                            print('No matching diagrams found')
+                        continue
+                    case '2':
+                        print('\nSearch parameters \n---------------')
+                        min_width = input('Min width (enter blank for 0): ')
+                        max_width = input('Max width (enter blank for max): ')
+                        min_height = input('Min height (enter blank for zero): ')
+                        max_height = input('Max height (enter blank for max): ')
+                        valid = False
+                        while not valid:
+                            difficult = input('Difficult (yes/no/All): ')
+                            if difficult.lower() in ['y', 'yes', 'n', 'no', 'a', 'all']:
+                                valid = True
+                                continue
+                            print('Invalid input, try again')
+
+                        valid = False
+                        while not valid:
+                            truncated = input('Truncated (yes/no/All): ')
+                            if truncated.lower() in ['y', 'yes', 'n', 'no', 'a', 'all']:
+                                valid = True
+                                continue
+                            print('Invalid input, try again')
+
+
+                        found = {}
+
+                        min_width = int(min_width) if min_width else 0
+                        max_width = int(max_width) if max_width else float('inf')
+                        min_height = int(min_height) if min_height else 0
+                        max_height = int(max_height) if max_height else float('inf')
+                        if truncated.lower() in ['y', 'yes']:
+                            truncated = 1
+                        elif truncated.lower() in ['n', 'no']:
+                            truncated = 0
+
+                        if difficult.lower() in ['y', 'yes']:
+                            difficult = 1
+                        elif difficult.lower() in ['n', 'no']:
+                            difficult = 0
+
+                        for name, diagram in diagrams.items():
+                            if not (max_width >= diagram.width >= min_width):
+                                continue #onto next diagram
+                            if not (max_height >= diagram.height >= min_height):
+                                continue
+                            if truncated in ['1', '0']:
+                                if truncated != diagram.truncated:
+                                    continue
+                            if difficult in ['1', '0']:
+                                if difficult != diagram.difficult:
+                                    continue
+                            found[name] = diagram
+
+                        if len(found) > 0:
+                            print(f'\nFound {len(found)} diagrams: ')
+                            print(', \n'.join(found.keys()))
+                        else:
+                            print('No matching diagrams found')
                         continue # todo: consistency in search format (.xml or no .xml)
+
                     # default???
-            case "6":
-                print("6")
+            case '6':
+                valid = False
+                while not valid:
+                    stat = input('\nAvailable statistics: \n\t1) Number of loaded diagrams\n\t2) Total number of total objects\n\t3) Diagram Object Types (list names)\n\t4) Minimum and Maximum dimensions\n\t5) Minimum and Maximum object areas\nYour Selection: ')
+                    if 1 <= int(stat) <= 5:
+                        valid = True
+                    else:
+                        print('Invalid input...')
+
+                    if len(diagrams) == 0:
+                        print("No Diagrams loaded, load diagrams to access statistics")
+                        continue # correct control flow?
+
+                    match stat:
+                        case '1':
+                            print(f'\nThere are currently {len(diagrams)} diagrams loaded.\n')
+                            continue
+                        case '2':
+                            ctr = 0
+                            for name, diagram in diagrams.items():
+                                ctr += len(diagram.objects)
+                            print(f'\nTotal of objects: {ctr}\n')
+                            continue
+                        case '3':
+                            obj_types = []
+
+                            for name, diagram in diagrams.items():
+                                for obj in diagram.objects:
+                                    if obj.objecttype not in obj_types:
+                                        obj_types.append(obj.objecttype)
+                            print('\nAll object types:')
+                            for name in obj_types:
+                                print(f'\t- {name}')
+                            print()
+                            continue
+                        case '4':
+                            min_w_diagram = 'none'
+                            max_w_diagram = 'none'
+                            min_h_diagram = 'none'
+                            max_h_diagram = 'none'
+
+                            min_width = float('inf')
+                            max_width = 0
+                            min_height = float('inf')
+                            max_height = 0
+
+                            for name, diagram in diagrams.items():
+                                if diagram.width < min_width:
+                                    min_w_diagram = name
+                                    min_width = diagram.width
+                                if diagram.width > max_width:
+                                    max_w_diagram = name
+                                    max_width = diagram.width
+                                if diagram.height < min_height:
+                                    min_h_diagram = name
+                                    min_height = diagram.height
+                                if diagram.height > max_height:
+                                    max_h_diagram = name
+                                    max_height = diagram.height
+                            print(f'\nminimum width: {min_width} ({min_w_diagram}) \nmaximum width: {max_width} ({max_w_diagram}) \nminimum height: {min_height} ({min_h_diagram})\nmaximum height: {max_height} ({max_h_diagram})\n ')
+                            continue
+
+                        case '5':
+                            print('bla')
+                            continue
                 continue
-            case "7":
-                confirm = input("Are you sure you want to quit the program (Yes/No)? ")
-                match confirm.lower():
-                    case "y"|"yes":
-                        print("Good bye…")
+
+            case '7':
+                valid = False
+                while not valid:
+                    confirmation = input('Are you sure you want to quit the program (Yes/No)? ')
+                    if confirmation.lower() in ['y', 'yes', 'n', 'no']:
+                        valid = True
+                        continue
+                    print('Invalid entry'
+                          )
+                match confirmation.lower():
+                    case 'y'|'yes':
+                        print('Good bye…')
                         sys.exit()
                     case _:
                         continue
 
             # else say selection not valid, invalid inputs ++
-        if invalidInputs == 0:
-            print("That's not valid dingus")
-        elif invalidInputs == 1:
-            print("Again fuck face?...try again")
-        elif invalidInputs == 2:
-            print("Can you read? Theres 7 fucking options")
-        invalidInputs += 1
+        if invalid_inputs == 0:
+            print('That\'s not valid dingus')
+        elif invalid_inputs == 1:
+            print('Again fuck face?...try again')
+        elif invalid_inputs == 2:
+            print('Can you read? Theres 7 fucking options')
+        invalid_inputs += 1
 
-        if invalidInputs == 3:
-            print("No bueno, go fuck yourself")
+        if invalid_inputs == 3:
+            print('No bueno, go fuck yourself')
 
 main()
-
-
-
-
-
-
